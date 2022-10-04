75b7f86838ffabb5568396f9b999ced20cfc352f
seanbaraka <seanbaraka@gmail.com>
2022-10-04 16:22:08 +0300

    feat: register handlebars engine and added /get-started route

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const { engine } = require('express-handlebars');
-require('dotenv').configure();
+import express from 'express';
+import { engine } from 'express-handlebars';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const port = process.env.PORT;
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-	res.send('Yoh, niaje');
+	res.render('index', { title: 'CADS | A Digital Lending Solution'});
 });
+
+app.get('/get-started', (req, res) => {
+	res.render('register', { title: 'Account Creation'})
+})
 
 app.listen(port, () => {
 	console.log(`Server started successfuly on http://localhost:${port}`);
